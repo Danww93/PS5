e28bbc217fecfbee700649db829937f8e1b740b4
danww93 <danww@udel.edu>
2014-10-22 00:11:17 -0400

    PS5

--- PS5/src/com/example/ps5/MainActivity.java
+++ PS5/src/com/example/ps5/MainActivity.java
@@ -6,34 +6,52 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	Button button;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		addListenerOnButton();
 	}
 	
-	public void CalcFV(View view)
+	public void addListenerOnButton()
 	{
-		EditText editText1 = (EditText) findViewById(R.id.editText1);
-		EditText editText2 = (EditText) findViewById(R.id.editText2);
-		EditText editText3 = (EditText) findViewById(R.id.editText3);
-		String invest = editText1.getText().toString();
-		String yrs = editText2.getText().toString();
-		String IRate = editText3.getText().toString();
-		double inv = Double.parseDouble(invest);
-		int years = Integer.parseInt(yrs);
-		double IR = Double.parseDouble(IRate);
-		CalcFutureValue FV = new CalcFutureValue();
-		double total = CalcFutureValue.FutureValue(inv, years, IR);
-		String TFV = "Your Future Value is" + total;
+		button = (Button) findViewById(R.id.button1);
 		
-		
-		
-		
+		button.setOnClickListener(new OnClickListener()
+		{
+			
+			public void onClick(View arg0)
+			{
+				TextView textView = (TextView) findViewById(R.id.textView5);
+				EditText editText1 = (EditText) findViewById(R.id.editText1);
+				EditText editText2 = (EditText) findViewById(R.id.editText2);
+				EditText editText3 = (EditText) findViewById(R.id.editText3);
+				String invest = editText1.getText().toString();
+				String yrs = editText2.getText().toString();
+				String IRate = editText3.getText().toString();
+				double inv = Double.parseDouble(invest);
+				int years = Integer.parseInt(yrs);
+				double IR = Double.parseDouble(IRate);
+				CalcFutureValue FV = new CalcFutureValue();
+				double total = CalcFutureValue.FutureValue(inv, years, IR);
+				String TFV = "$" + total;
+				textView.setText(TFV);
+		    }
+			
+		});
 	}
+	
 }
